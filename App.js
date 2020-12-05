db41b110ec5c98621c13ff7f6b8949e4d4fe5f4a
Emre Polat <therasmus941@gmail.com>
2020-12-05 14:29:46 +0300

    Update App.js
    deneme

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello My App</Text>
+      <Text>React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
