0bd86622becac400a8776df2206485f01b2394a1
okaytonka <okaytonka@gmail.com>
2020-12-06 21:07:00 +0300

    Fix For Firebase Error

--- screens/HighScore.js
+++ screens/HighScore.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from '../Firebase';
 import Leaderboard from 'react-native-leaderboard';
+import { Alert } from 'react-native';
 
 export default class HighScore extends React.Component { //App
   constructor(){
@@ -13,12 +14,19 @@ export default class HighScore extends React.Component { //App
         //...
     ] //can also be an object of objects!: data: {a:{}, b:{}}
 }
-
+alert = (title, body) => {
+  Alert.alert(title, body, [{ text: "OK", onPress: () => {} }], {
+    cancelable: false
+  });
+};
   render(){
     return (
         <Leaderboard 
+        onRowPress={(item, index) => {
+          this.alert(item.name + " clicked", item.score + " points, wow!")}}
         data={this.state.data} 
         sortBy='highScore' 
-        labelBy='userName'/>)
+        labelBy='userName'/>
+        )
   }
 }
