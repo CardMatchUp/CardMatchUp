51478d37b15b662286e3a92879cdab34b287ea28
tibet_erol <TibetErol99@gmail.com>
2020-12-08 03:38:04 +0300

    made corrections in App.js

--- App.js
+++ App.js
@@ -8,25 +8,7 @@ import HighScore from "./screens/HighScore";
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-export function App() {
-
-  /*return  <Login />;*/
-  return <HighScore/>
-  
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 const AppNavigator = createStackNavigator({
-
-  
 
   Register: {
   screen: Register,
